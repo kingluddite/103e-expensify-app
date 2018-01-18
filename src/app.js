7f9ce9928955501135c48ac2d607682b16faed6e
kindluddite <howley.phil@gmail.com>
2018-01-18 06:18:42 -0800

    fix spacing issues

--- src/app.js
+++ src/app.js
@@ -1,23 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import "normalize.css/normalize.css";
-import AppRouter from "./routers/AppRouter";
-import configureStore from "./store/configureStore";
-import { addExpense } from "./actions/expenses";
-import { setTextFilter } from "./actions/filters";
-import getVisibleExpenses from "./selectors/expenses";
-import "./styles/styles.scss";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import 'normalize.css/normalize.css';
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+import './styles/styles.scss';
 
 const store = configureStore();
 
 // console.log(addExpense);
-store.dispatch(addExpense({ description: "Water bill", amount: 20000 }));
-store.dispatch(addExpense({ description: "Gas bill" }));
-store.dispatch(setTextFilter("water"));
+store.dispatch(addExpense({ description: 'Water bill', amount: 20000 }));
+store.dispatch(addExpense({ description: 'Gas bill' }));
+store.dispatch(setTextFilter('water'));
 
 setTimeout(() => {
-  store.dispatch(setTextFilter("bill"));
+  store.dispatch(setTextFilter('bill'));
 }, 3000);
 
 const state = store.getState();
@@ -32,4 +32,4 @@ const jsx = (
   </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById("app"));
+ReactDOM.render(jsx, document.getElementById('app'));
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { connect } from "react-redux";
-import ExpenseListItem from "./ExpenseListItem";
-import selectExpenses from "../selectors/expenses";
+import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseListItem from './ExpenseListItem';
+import selectExpenses from '../selectors/expenses';
 
 const ExpenseList = props => (
   <div>
@@ -13,7 +13,7 @@ const ExpenseList = props => (
 );
 
 const mapStateToProps = state => ({
-  expenses: selectExpenses(state.expenses, state.filters)
+  expenses: selectExpenses(state.expenses, state.filters),
 });
 
 export default connect(mapStateToProps)(ExpenseList);
