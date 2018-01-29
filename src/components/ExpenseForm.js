9759d623408a5a75ea02016049cff4751e89b432
kindluddite <howley.phil@gmail.com>
2018-01-29 14:50:11 -0800

    test form submit and inline events

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -3,7 +3,6 @@ import moment from 'moment';
 import 'react-dates/initialize';
 import { SingleDatePicker } from 'react-dates';
 
-const now = moment();
 export default class ExpenseForm extends React.Component {
   constructor(props) {
     super(props);
